fce16337998c6e37ee7c83fed62485f2b148f910
Tristan Rice <rice@fn.lc>
2019-09-09 20:38:25 -0700

    updated to use newer torchscript api

--- cgo.go
+++ cgo.go
@@ -1,6 +1,6 @@
 package gotorch
 
 // #cgo CPPFLAGS: -I/usr/lib/python3.7/site-packages/torch/lib/include/
-// #cgo LDFLAGS: -L/usr/lib/python3.7/site-packages/torch/lib -ltorch -lcaffe2 -lc10
+// #cgo LDFLAGS: -L/usr/lib/python3.7/site-packages/torch/lib -ltorch -lc10
 // #include "gotorch.h"
 import "C"
--- gotorch.cc
+++ gotorch.cc
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <memory>
+#include <sstream>
 
 using namespace torch;
 
@@ -12,22 +13,22 @@ TorchJitCompileResult TorchJitCompile(char* cscript) {
   TorchJitCompileResult res = {};
   std::string script(cscript);
   try {
-    std::shared_ptr<jit::script::Module> module = jit::compile(script);
-    assert(module != nullptr);
-    res.res = (void*)new std::shared_ptr<jit::script::Module>(module);
-  } catch (std::exception& e) {
+    auto cu = jit::compile(script);
+    assert(cu != nullptr);
+    res.res = (void*)new jit::script::CompilationUnit(std::move(*cu));
+  } catch (const std::exception& e) {
     res.err = strdup(e.what());
   }
   return res;
 }
 
 void TorchJitScriptModuleDelete(TorchJitScriptModule mptr) {
-  delete static_cast<std::shared_ptr<jit::script::Module>*>(mptr);
+  delete static_cast<jit::script::CompilationUnit*>(mptr);
 }
 
 AtTensor TorchJitScriptModuleRunMethod(TorchJitScriptModule mptr, char* cmethod,
-                                       AtTensor* valsptr, int num_vals) {
-  auto* m = static_cast<std::shared_ptr<jit::script::Module>*>(mptr);
+				       AtTensor* valsptr, int num_vals) {
+  auto* m = static_cast<jit::script::CompilationUnit*>(mptr);
   std::vector<IValue> vals;
   vals.reserve(num_vals);
   for (int i = 0; i < num_vals; i++) {
@@ -35,15 +36,18 @@ AtTensor TorchJitScriptModuleRunMethod(TorchJitScriptModule mptr, char* cmethod,
     vals.push_back(*val);
   }
   std::string method(cmethod);
-  auto res = (*m)->get_method(method)(vals);
+  auto res = m->get_function(method)(vals);
   return (void*)new at::Tensor(res.toTensor());
 }
 
+void tensorDeleter(void* ptr) { free(ptr); }
+
 AtTensor TorchTensorFromBlob(void* data_ptr, int64_t* sizes_ptr,
-                             int sizes_len) {
+			     int sizes_len) {
   std::vector<int64_t> sizes(sizes_ptr, sizes_ptr + sizes_len);
-  return (void*)new at::Tensor(torch::autograd::make_variable(
-      at::getType(at::kFloat).tensorFromBlob(data_ptr, sizes)));
+  at::TensorOptions opts(torch::kFloat32);
+  return (void*)new torch::Tensor(
+      torch::from_blob(data_ptr, sizes, tensorDeleter, opts));
 }
 
 void TorchTensorDelete(AtTensor tptr) { delete static_cast<at::Tensor*>(tptr); }
@@ -56,7 +60,7 @@ int TorchTensorDim(AtTensor tptr) {
 void TorchTensorSizes(AtTensor tptr, int64_t* data) {
   auto* t = static_cast<at::Tensor*>(tptr);
   auto sizes = t->sizes();
-  for (int i = 0; i < sizes.size(); i++) {
+  for (size_t i = 0; i < sizes.size(); i++) {
     data[i] = sizes.at(i);
   }
 }
